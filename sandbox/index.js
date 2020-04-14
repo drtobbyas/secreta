ae0881582716c65c03007467179163cba2f4b8ac
drtobbyas <dr.tobbyas@gmail.com>
2020-04-14 11:49:10 +0100

    add config file

---
+++ sandbox/index.js
@@ -0,0 +1,8 @@
+
+const { config } = require('secreta')
+// const { load } = require('secreta')
+
+// const config = load({key: '1234567', environment: process.env.NODE_ENV})
+
+console.log(config)
+console.log(process.argv[2])
